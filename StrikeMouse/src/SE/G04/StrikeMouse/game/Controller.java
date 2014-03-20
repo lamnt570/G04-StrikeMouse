671223999fb4b5e607e5e57d0c67d138eb0545a6
lamnt_570 <ntlam94@gmail.com>
2014-03-20 16:10:57 +0700

    Update Controller, Renderer class

--- StrikeMouse/src/SE/G04/StrikeMouse/game/Controller.java
+++ StrikeMouse/src/SE/G04/StrikeMouse/game/Controller.java
@@ -1,11 +1,24 @@
 package SE.G04.StrikeMouse.game;
 
+import SE.G04.StrikeMouse.util.Constants;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 public class Controller {
 	private static final String TAG =  Controller.class.getName();
+	public Sprite backGround;
 	
-	public Controller () {}
+	public Controller () {
+		init();
+	}
 	
-	private void init () {}
+	private void init () {
+		backGround = new Sprite(Assets.instance.backGround.backGround);
+		backGround.setSize(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+		backGround.translate(-Constants.VIEWPORT_WIDTH/2, -Constants.VIEWPORT_HEIGHT/2);
+	}
 	
-	public void update (float deltaTime) {}
+	public void update (float deltaTime) {
+		
+	}
 }
